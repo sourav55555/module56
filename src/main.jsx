c09884a0a317e5d19c6e6e384d179969a1be9b83
sourav55555 <souravsinghassr555@gmail.com>
2023-04-06 20:34:10 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Component/Layout/Main';
+import Home from './Component/Home/Home';
+import OrderReview from './Component/OrderReview/OrderReview';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "review",
+        element: <OrderReview/>
+      }
+    ]
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
